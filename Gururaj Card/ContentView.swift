1b477d7101c6e21cfcd1f81c602dde951f289eec
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 21:33:33 +0530

    Added the info view

--- Gururaj Card/ContentView.swift
+++ Gururaj Card/ContentView.swift
@@ -13,11 +13,12 @@ struct ContentView: View {
             Color(.green).opacity(0.8).ignoresSafeArea()
             VStack{
                 Image("guruImage")
-                    .resizable().aspectRatio(contentMode: .fit)
-                    .cornerRadius(60)
+                    .resizable().aspectRatio(contentMode: .fill)
                     .frame(width: 170, height: 170)
-                    
-                   
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(style: StrokeStyle(lineWidth: 5)).foregroundColor(.white)
+                    }
                 Text("Gururaja")
                     .font(.custom("PirataOne-Regular", size: 40))
                     .bold()
@@ -25,12 +26,38 @@ struct ContentView: View {
                 Text("Ios Developer")
                     .font(.custom("Poppins-Regular", size: 25))
                     .foregroundColor(.white)
+                Divider()
+                    .foregroundColor(.white)
+                infoView(image: "phone.fill", text: "+91 9008771821")
+                infoView(image: "envelope.fill", text: "gururajkumar161@gmail.com")
             }
+            
         }
-        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct infoView: View {
+    
+    let image: String
+    let text: String
+    var body: some View {
+        VStack{
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height:50).padding(.horizontal)
+                .foregroundColor(.white)
+                .overlay {
+                    HStack{
+                        Image(systemName: image)
+                            .foregroundColor(.green)
+                        Text(text)
+                            .font(.system(size: 20))
+                    }
+                    
+                }
+        }
+    }
 }
