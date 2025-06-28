f9b6a6abca6e6a4eca5a2ca2f86ae6c16a93b3e7
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 20:15:30 +0530

    Initial Commit

---
+++ Gururaj Card/Gururaj_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gururaj_CardApp.swift
+//  Gururaj Card
+//
+//  Created by Gururaja M on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Gururaj_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
